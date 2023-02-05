03237126c500f3d73e3b63076cf60e38beb03b2c
Ilgiz linux <ilgizahiyarov95@gmail.com>
2023-02-05 20:21:40 +0500

    wrong database model

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -6,7 +6,10 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
+
+    student_oject = Student.objects.all()
+
+    context = {'object_list': student_oject}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
