bfe4036f59ef77c2527291b4a415c84e980a1cee
Ilgiz linux <ilgizahiyarov95@gmail.com>
2023-02-12 21:14:25 +0500

    many_to_many

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,49 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, ArticleScope, Tag
+
+
+
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        activated_checkboxes = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                activated_checkboxes += 1
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+            # raise ValidationError('Тут всегда ошибка')
+        if activated_checkboxes == 0:
+            raise ValidationError('Укажите основной раздел')
+        if activated_checkboxes > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+
+class RelationshipInline(admin.TabularInline):
+    model = ArticleScope
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
+    
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
+
+
+
+
+
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,16 +1,56 @@
 from django.db import models
 
 
+class Tag(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name='Тэг')
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        
+
+    def __str__(self):
+        return self.name
+
+
+    
+
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
+
+    article_scope = models.ManyToManyField(Tag, through='ArticleScope')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+
+
+
+class ArticleScope(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='раздел')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(verbose_name='основной')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
+        ordering = ('-is_main', 'tag__name')
+        
+
+    
+
+
+
+
